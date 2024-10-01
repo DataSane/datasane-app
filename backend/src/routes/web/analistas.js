05f0a5e2a05e1eaea2b6704a4a492acb5d8b1e1f
jeancharlesx64 <jean.albuquerque@sptech.school>
2024-10-01 20:27:34 -0300

    [FIX] :: Fixing the router of analista

--- backend/src/routes/web/analistas.js
+++ backend/src/routes/web/analistas.js
@@ -17,6 +17,44 @@ router.get('/', async function(req, res) {
     }
 });
 
+// Rota raiz dentro do grupo /dashboard
+router.get('/iframe', (req, res) =>{
+    if (req.session.authenticated) {
+
+        let hasError = false;
+        let errorMessage = '';
+
+        let hasBeenRegistered = false;
+
+        if(req.session.hasError){
+            hasError = true;
+            errorMessage = req.session.errorMessage;
+
+            delete req.session.hasError;
+            delete req.session.errorMessage;
+        }
+
+        if(req.session.hasBeenRegistered){
+            hasBeenRegistered = true;
+            delete req.session.hasBeenRegistered
+        }
+        
+        // dados do usuário
+        const user = req.session.user;
+        res.render('analistasIframe', {
+            userId: user.session_userId,
+            hasError: hasError,
+            errorMessage: errorMessage,
+            hasBeenRegistered: hasBeenRegistered
+        });
+
+    }else{
+        req.session.hasError = true;
+        req.session.errorMessage = 'Faça login antes para registrar monitores!'
+        res.redirect('/');
+    }
+});
+
 // router.get('/fazenda/:fazendaId', function(req, res){
 //     if (req.session.authenticated) {
 
