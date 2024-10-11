77778cc88412c4f202c847825109b8a1158b4513
jeancharlesx64 <jean.albuquerque@sptech.school>
2024-10-11 11:15:10 -0300

    [FEAT] :: Adding the report URI, and anchoring the view page

--- backend/src/routes/web/index.js
+++ backend/src/routes/web/index.js
@@ -6,10 +6,12 @@ const loginRoutes = require('./login');
 const dashboardRoutes = require('./dashboard');
 const analistasRoutes = require('./analistas');
 const municipiosRoutes = require('./municipios');
+const relatoriosRoutes = require('./relatorios');
 
 router.use('/', loginRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/analistas', analistasRoutes);
-router.use('/municipios', municipiosRoutes)
+router.use('/municipios', municipiosRoutes);
+router.use('/relatorios', relatoriosRoutes);
 
 module.exports = router;
---
+++ backend/src/routes/web/relatorios.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+// Rota raiz dentro do grupo /dashboard
+router.get('/', async function(req, res) {
+    if (req.session.authenticated) {
+
+        res.render('desempenho', {
+            userid: req.session.user.session_userid,
+            isadmin: req.session.user.session_isadmin
+        });
+
+    }else{
+        req.session.hasError = true;
+        req.session.errorMessage = 'Faça login antes de acessar a dashboard!'
+        res.redirect('/');
+    }
+});
+
+module.exports = router;
