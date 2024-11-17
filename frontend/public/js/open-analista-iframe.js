f093328c286165f3af9dc0041b72dc48d745a9a3
João <joao.melo@sptech.school>
2024-11-17 19:59:13 -0300

    Modal para confirmação de delete adicionado

--- frontend/public/js/open-analista-iframe.js
+++ frontend/public/js/open-analista-iframe.js
@@ -1,8 +1,13 @@
 var openModalAdicionarAnalista = document.querySelector('#openModalAdicionarAnalista')
+var openModalEditarAnalista = document.querySelector('#openModalEditarAnalista')
 var modalAdicionarAnalista = document.querySelector('#modalAdicionarAnalista');
 var closeAdicionarAnalista = document.querySelector('#closeAdicionarAnalista');
     
 openModalAdicionarAnalista.addEventListener('click', function(){
+    modalAdicionarAnalista.classList.toggle('active');
+})
+
+openModalEditarAnalista.addEventListener('click', function(){
     modalAdicionarAnalista.classList.toggle('active');
 })
 
