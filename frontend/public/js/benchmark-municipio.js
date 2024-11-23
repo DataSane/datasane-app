a40619848dc7a1d82ba4b6db150e5b1c2722945b
jeancharlesx64 <jean.albuquerque@sptech.school>
2024-11-23 16:12:59 -0300

    [FEAT] :: Options dinamically, search bar is working also

--- frontend/public/js/benchmark-municipio.js
+++ frontend/public/js/benchmark-municipio.js
@@ -4,18 +4,34 @@ document.addEventListener('DOMContentLoaded', function () {
     
     const url = `${actualIP}${municipiosEndpoint}`;
 
-    async function fetchAndLogMunicipios() {
+    async function fetchAndRenderMunicipios() {
         try {
             const response = await fetch(url);
             if (!response.ok) {
                 throw new Error('Error in getMunicipios');
             }
             const municipios = await response.json();
-            console.log(municipios);
+            renderMunicipiosInDatalists(municipios);
         } catch (error) {
             console.error('Fetch error:', error);
         }
     }
 
-    fetchAndLogMunicipios();
+    function renderMunicipiosInDatalists(municipios) {
+        const datalist1 = document.getElementById('municipio1');
+        const datalist2 = document.getElementById('municipio2');
+
+        municipios.forEach(municipio => {
+            const option1 = document.createElement('option');
+            option1.value = municipio.nome;
+
+            const option2 = document.createElement('option');
+            option2.value = municipio.nome;
+
+            datalist1.appendChild(option1);
+            datalist2.appendChild(option2);
+        });
+    }
+
+    fetchAndRenderMunicipios();
 });
