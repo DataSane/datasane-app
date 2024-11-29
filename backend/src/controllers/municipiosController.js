d0a8012b2e22e5ccea31fa3d64fbf3c9ac3f67af
Kauan França <kauan.franca@sptech.school>
2024-11-29 00:57:39 -0300

    fixing-municipios-endpoints-routers

--- backend/src/controllers/municipiosController.js
+++ backend/src/controllers/municipiosController.js
@@ -1,8 +1,18 @@
 var municipiosModel = require('../models/municipiosModel');
 
-async function getMunicipios(porteMunicipio) {
+async function getMunicipios(req, res) {
     try {
-        const municipios = await municipiosModel.municipios(porteMunicipio); // Pela quantidade de dados, necessário utilização de await e async
+        const municipios = await municipiosModel.municipios(); // Pela quantidade de dados, necessário utilização de await e async
+        return municipios;
+    } catch (err) {
+        console.error('Error trying to catch Municípios:', err);
+        throw err;
+    }
+}
+
+async function getMunicipiosWithoutFilters(porteMunicipio) {
+    try {
+        const municipios = await municipiosModel.municipiosWithoutFilters(porteMunicipio); // Pela quantidade de dados, necessário utilização de await e async
         return municipios;
     } catch (err) {
         console.error('Error trying to catch Municípios:', err);
@@ -19,4 +29,4 @@ async function getMunicipioById(id){
     const municipio = await municipiosModel.findMunicipioById(id);
     return municipio;
 }
-module.exports = { getMunicipios, getMunicipiosFiltered, getMunicipioById };
+module.exports = { getMunicipios, getMunicipiosWithoutFilters, getMunicipiosFiltered, getMunicipioById };
